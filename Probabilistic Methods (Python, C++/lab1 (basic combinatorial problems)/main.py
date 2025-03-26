89a12385059260f162218dee8806f8846119672b
p1467864 <s197776@student.pg.edu.pl>
2025-03-27 00:21:41 +0100

    Lab1 PM finished

--- Probabilistic Methods (Python, C++/lab1 (basic combinatorial problems)/main.py
+++ Probabilistic Methods (Python, C++/lab1 (basic combinatorial problems)/main.py
@@ -2,39 +2,22 @@ from util import *
 from data import *
 
 cities = load_cities("France.txt")
-N, M = 3, 2
 
-i = 1
-# 1) Dla podanej liczby N wypisać ponumerowane wszystkie porządki odwiedzin N miast 1,2,...,N
-for p in permutations(cities, N):
-    city_names = [city['name'] for city in p]
-    print(i, city_names)
-    i += 1
-print("")
+N, M = 5, 3
 
-# Dla podanych liczb N i M wypisać ponumerowane wszystkie porządki odwiedzin M z N miast 1,2,...,N
-# Dla podanych liczb N i K <= N wypisać ponumerowane wszystkie podzbiory K z N miast 1,2,...,N
-i = 1
-for c in combinations(cities, N, M):
-    city_names = [city['name'] for city in c]
-    print(i, city_names)
-    i += 1
-print("")
+print("\nOczekiwane: ", factorial(N) / factorial(N - M))
+for i, p in enumerate(permutations_m_from_n(cities, N, M), 1):
+     print(i, [city["name"] for city in p])
 
-# Dla podanych liczb N i M wypisać ponumerowane wszystkie podzbiory, z możliwymi powtórzeniami, M z N miast 1,2,...,N
-i = 1
-for m in multisets(cities, N, M):
-    city_names = [city['name'] for city in m]
-    print(i, city_names)
-    i += 1
-print("")
+print("\nOczekiwane: ", newton(M + N - 1, N - 1))
+for i, m in enumerate(multisets(cities, N, M), 1):
+     print(i, [city["name"] for city in m])
 
-# Korzystając z informacji o współrzędnych x i y miast, podać przebieg i długość najkrótszej trasy-cyklu odwiedzin miast.
-route, distance = shortest_route(cities, N)
-print("Najkrótsza trasa:", [city["name"] for city in route])
-print("Długość trasy:", distance, "km")
-print("")
+N, M = 7, 5
 
-# Korzystając z informacji o liczbie ludności miast, podać podzbiór, dla którego sumaryczna liczba mieszkańców jest najbliższa 50% liczby mieszkańców (bez powtórzeń) N miast.
-best_comb = closest_population_comb(cities, N)
-print("Najlepszy podzbiór:", [city["name"] for city in best_comb])
+route, distance = shortest_route(cities, N, M)[0]
+print("\nMiasta:", [city["name"] for city in route])
+print("Długość trasy:", distance)
+
+probability = population_probability(cities, N, M)
+print("\nPrawdopodobieństwo:", probability)
--- Probabilistic Methods (Python, C++/lab1 (basic combinatorial problems)/util.py
+++ Probabilistic Methods (Python, C++/lab1 (basic combinatorial problems)/util.py
@@ -1,72 +1,62 @@
 import math
 
-def permutations(elements, N):
-    if N == 0:
-        return [[]]
-    elements = elements[:N]
-    generated = []
-    for i in range(N):
-        rest = elements[:i] + elements[i+1:]
-        for p in permutations(rest, len(rest)):
-            generated.append([elements[i]] + p)
-    return generated
+def factorial(n) -> int:
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
 
-def combinations(elements, N, K):
-    if K == 0:
+def newton(n, k) -> int:
+    return factorial(n) / (factorial(k) * factorial(n-k))
+
+def permutations_m_from_n(elements, N, M):
+    if M == 0:
         return [[]]
-    if N < K or N == 0:
+    if N < M or N == 0:
         return []
     elements = elements[:N]
     generated = []
     for i in range(N):
-        rest = elements[i+1:]
-        for c in combinations(rest, len(rest), K-1):
-            generated.append([elements[i]] + c)
+        rest = elements[:i] + elements[i + 1:]
+        for p in permutations_m_from_n(rest, len(rest), M - 1):
+            generated.append([elements[i]] + p)
     return generated
 
-def multisets(elements, N, K):
-    if K == 0 or N == 0:
+def multisets(elements, N, M):
+    if M == 0 or N == 0:
         return [[]]
     elements = elements[:N]
     generated = []
     for i in range(N):
         rest = elements[i:]
-        for m in multisets(rest, len(rest), K-1):
+        for m in multisets(rest, len(rest), M - 1):
             generated.append([elements[i]] + m)
     return generated
 
-def radians(deg):
-    return deg * math.pi / 180
+def distance(latitude1, latitude2, longitude1, longitude2):
+    return math.sqrt((latitude2 - latitude1)**2 + (longitude2 - longitude1)**2)
 
-def haversine(latitude1, longitude1, latitude2, longitude2):
-    phi1, phi2 = radians(latitude1), radians(latitude2)
-    diff_phi = radians(latitude2 - latitude1)
-    diff_lambda = radians(longitude2 - longitude1)
-    alfa = (math.sin(diff_phi/2))**2 + math.cos(phi1) * math.cos(phi2) * (math.sin(diff_lambda/2))**2
-    return 2 * 6371 * math.asin(math.sqrt(alfa))
+def shortest_route(cities, N, M):
+    routes = permutations_m_from_n(cities, N, M)
+    distances = []
+    for route in routes:
+        current_distance = 0
+        for i in range(M - 1):
+            current_distance += distance(route[i]['latitude'], route[i + 1]['latitude'], route[i]['longitude'], route[i + 1]['longitude'])
+        current_distance += distance(route[M - 1]['latitude'], route[0]['latitude'], route[M - 1]['longitude'], route[0]['longitude'])
+        route.append(route[0])
+        distances.append((route, current_distance))
+    distances.sort(key=lambda x: x[1])
+    return distances
 
-def shortest_route(cities, N):
-    cities = cities[:N]
-    best_route, min_distance = None, float('inf')
-    for p in permutations(cities, N):
-        distance = 0
-        for i in range(len(p) - 1):
-            distance += haversine(p[i]['latitude'], p[i]['longitude'], p[i+1]['latitude'], p[i+1]['longitude'])
-        distance += haversine(p[-1]['latitude'], p[-1]['longitude'], p[0]['latitude'], p[0]['longitude'])
-        if distance < min_distance:
-            min_distance = distance
-            best_route = p
-    return best_route, min_distance
+def population_probability(cities, N, M):
+    subsets = multisets(cities, N, M)
+    sum_all = sum(city['population'] for city in cities[:N])
+    valid_subsets = 0
+    for subset in subsets:
+        unique_subset = list({city['id']: city for city in subset}.values())
+        current_sum = sum(city['population'] for city in unique_subset)
+        if 0.4 * sum_all <= current_sum <= 0.6 * sum_all:
+            valid_subsets += 1
+    return valid_subsets / sum(1 for _ in subsets)
 
-def closest_population_comb(cities, N):
-    cities = cities[:N]
-    total_population = sum(city['population'] for city in cities)
-    target = total_population / 2
-    best_c, best_diff = None, float('inf')
-    for r in range(1, len(cities) + 1):
-        for c in combinations(cities, N, r):
-            c_population = sum(city['population'] for city in c)
-            diff = abs(c_population - target)
-            if diff < best_diff:
-                best_diff, best_c = diff, c
-    return best_c
