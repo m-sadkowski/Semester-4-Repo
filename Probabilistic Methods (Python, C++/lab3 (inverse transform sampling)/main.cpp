b9a006fdadc16f76e033f70c222a275a1775f4c8
Michał Sadkowski <msadkowski000@gmail.com>
2025-04-10 17:59:49 +0200

    PM lab3 elimination method

--- Probabilistic Methods (Python, C++/lab3 (inverse transform sampling)/main.cpp
+++ Probabilistic Methods (Python, C++/lab3 (inverse transform sampling)/main.cpp
@@ -1,66 +1,85 @@
-﻿#include <iostream>
+#include <iostream>
 #include <random>
 #include <vector>
 #define N 100000
 
 using namespace std;
 
-extern "C" double transformUtoY(double);
+double transformUtoY(double u) {
+    return 100 * u + 50;
+}
 
-/*
-1.
-Wygenerowac rzeczywiste <50,150> 10 podprzedzialow
-2.  P(1)=0,05
-    P(2)=0,15
-    P(3)=0,25
-    P(4)=0,55
-*/
+double mapU(double y) {
+    if (y <= 0.2) return 1;
+    if (y <= 0.6) return 2;
+    if (y <= 0.9) return 3;
+    return 4;
+}
 
-double mapU(double y)
-{
-    // dyskretny wybór wartości na podstawie skumulowanych prawdopodobieństw.
-    if (y <= 0.05) return 1; // P(Y=1) = 0.05
-    if (y <= 0.2) return 2; // P(Y=2) = 0.15 (suma 0.05+0.15)
-    if (y <= 0.45) return 3; // P(Y=3) = 0.25 (suma 0.2+0.25)
-    return 4; // P(Y=4) = 0.55 (reszta)
+double density(double x) {
+    return (x - 50) / 5000;
 }
 
 int main()
 {
-    // Losowane jest N liczby z przedziału [0, 1] za pomocą generatora mt19937.
     random_device rand_dev;
     mt19937 generator(rand_dev());
+    double fmax = 0.02;
     uniform_real_distribution<double> dist(0.0, 1.0);
+    uniform_real_distribution<double> dist1(0.0, fmax);
+    uniform_real_distribution<double> distDensity(50.0, 150.0);
 
     vector<double> numbersA;
     vector<int> numbersB;
-    numbersA.reserve(N);
-    numbersB.reserve(N);
-    vector<int> distributionA(10, 0); // 10 przedziałów [50-150)
-    vector<int> distributionB(4, 0);  // 4 kategorie
+    vector<double> selected;
+    vector<int> distElimination(10, 0);
+    int tries = 0;
+    vector<int> distA(10, 0);
+    vector<int> distB(4, 0);
 
     for (int i = 0; i < N; i++) {
-        numbersA.push_back(transformUtoY(dist(generator))); // 3.1 Y = F⁻¹(U) - odwracanie dystrybuanty dla rozkładu ciągłego F(y) = (y-50) / 100 dla y ∈ [50, 150]
-        numbersB.push_back(mapU(dist(generator))); // 3.3 wybiera najmniejsze k takie, że U ≤ ∑P(Y=i) dla i ≤ k - odwracania dystrybuanty dla rozkładu dyskretnego
+        numbersA.push_back(transformUtoY(dist(generator)));
+        numbersB.push_back(mapU(dist(generator)));
     }
 
     for (double number : numbersA) {
-        int i = (number - 50) / 10; // Oblicz indeks przedziału
-        distributionA[i]++;
+        int i = (number - 50) / 10;
+        distA[i]++;
     }
 
     for (int number : numbersB) {
-        distributionB[number - 1]++;
+        distB[number - 1]++;
+    }
+
+    while (selected.size() < N) {
+        double x = distDensity(generator); 
+        double u = dist1(generator);
+        double fx = density(x);
+        tries++;
+        if (u + 0.0001 <= fx) { 
+            selected.push_back(x);
+        }
+    }
+
+    for (double number : selected) {
+        int index = (number - 50) / 10;
+        distElimination[index]++;
     }
 
     cout << "A: " << endl;
     for (int i = 0; i < 10; i++) {
-        cout << 50 + 10 * i << " - " << 50 + 10 * (i + 1) << ": " << distributionA[i] << endl;
+        cout << 50 + 10 * i << " - " << 50 + 10 * (i + 1) << ": " << distA[i] << endl;
     }
 
     cout << endl << "B: " << endl;
     for (int i = 0; i < 4; i++) {
-        cout << i + 1 << ": " << distributionB[i] << endl;
+        cout << i + 1 << ": " << distB[i] << endl;
     }
-}
 
+    cout << endl << "eliminacja: " << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << 50 + 10 * i << " - " << 50 + 10 * (i + 1) << ": " << distElimination[i] << endl;
+    }
+
+    cout << endl << "Proby: " << tries << endl;
+}
